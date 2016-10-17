bbfb4aed86c07d98a722868ea1c8e9b1a021fcbe
pdebala <pdebala@sygnity.pl>
2016-10-17 12:16:20 +0200

    reverting configuration which lets to run app like war on external tomcat

--- src/main/java/pl/pdebala/eniro/EniroTaskApplication.java
+++ src/main/java/pl/pdebala/eniro/EniroTaskApplication.java
@@ -1,22 +1,39 @@
 package pl.pdebala.eniro;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.AsyncRestTemplate;
 
+import java.util.concurrent.Executor;
+
+/**
+ * When we want to run SpringBoot Application like war on Tomcat server we have to
+ * extends class {@link SpringBootServletInitializer}, and overload method {@link SpringBootServletInitializer#configure(SpringApplicationBuilder)}
+ * e.g in this way:
+ * {@code :}
+ *  <pre class="code">
+ *   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+ *       return application.sources(EniroTaskApplication.class);
+ *   }
+ * </pre>
+ *
+ * Additionally you have to change packaging type form 'jar' to 'war' in pom.xml file,
+ * and set tomcat starter dependency as a provided.
+ *
+ * If you want to run SpringBoot app on embedded tomcat, but different one than delivered with
+ * springBoot starters, you can use tomcat7-maven-plugin in your pom.xml file.
+ * Running such server is very simple, you need just use command: mvn tomcat7:run
+ *
+ */
 @SpringBootApplication
 @EnableAsync
-public class EniroTaskApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EniroTaskApplication.class);
-    }
+public class EniroTaskApplication extends AsyncConfigurerSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(EniroTaskApplication.class, args);
